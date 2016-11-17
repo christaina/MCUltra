6298567d8e62a19907e78c478a13612770d28ec9
MechCoder <manojkumarsivaraj334@gmail.com>
2016-11-17 14:28:42 -0500

    Consistent tabs

--- context_baseline.py
+++ context_baseline.py
@@ -159,184 +159,182 @@ class PTBModel(object):
         tf.float32, shape=[], name="new_learning_rate")
     self._lr_update = tf.assign(self._lr, self._new_lr)
 
-  def assign_lr(self, session, lr_value):
-    session.run(self._lr_update, feed_dict={self._new_lr: lr_value})
-  """
-  @property
-  def input(self):
-    return self._input
-  """
-  @property
-  def initial_state(self):
-    return self._initial_state
+    def assign_lr(self, session, lr_value):
+        session.run(self._lr_update, feed_dict={self._new_lr: lr_value})
 
-  @property
-  def acc(self):
-    return self._acc
-  @property
-  def c_initial_state(self):
-    return self._c_initial_state
+    @property
+    def initial_state(self):
+        return self._initial_state
 
-  @property
-  def predictions(self):
-    return self._predictions
+    @property
+    def acc(self):
+        return self._acc
 
-  @property
-  def logits(self):
-    return self._logits
+    @property
+    def c_initial_state(self):
+        return self._c_initial_state
 
-  @property
-  def cost(self):
-    return self._cost
+    @property
+    def predictions(self):
+        return self._predictions
 
-  @property
-  def targets(self):
-    return self.input_y
-  @property
-  def final_state(self):
-    return self._final_state
+    @property
+    def logits(self):
+        return self._logits
 
-  @property
-  def lr(self):
-    return self._lr
+    @property
+    def cost(self):
+        return self._cost
 
-  @property
-  def train_op(self):
-    return self._train_op
+    @property
+    def targets(self):
+        return self.input_y
+
+    @property
+    def final_state(self):
+        return self._final_state
+
+    @property
+    def lr(self):
+        return self._lr
+
+    @property
+    def train_op(self):
+        return self._train_op
 
 
 class SmallConfig(object):
-  """Small config."""
-  init_scale = 0.1
-  learning_rate = 0.01
-  max_grad_norm = 5
-  num_layers = 2
-  num_steps = 70
-  hidden_size = 200
-  max_epoch = 4
-  max_max_epoch = 13
-  keep_prob = 1.0
-  lr_decay = 0.5
-  batch_size = 32
-  vocab_size = 13065
+    """Small config."""
+    init_scale = 0.1
+    learning_rate = 0.01
+    max_grad_norm = 5
+    num_layers = 2
+    num_steps = 70
+    hidden_size = 200
+    max_epoch = 4
+    max_max_epoch = 13
+    keep_prob = 1.0
+    lr_decay = 0.5
+    batch_size = 32
+    vocab_size = 13065
 
 class TestConfig(object):
-  """Tiny config, for testing."""
-  init_scale = 0.1
-  learning_rate = 1.0
-  max_grad_norm = 1
-  num_layers = 1
-  num_steps = 2
-  hidden_size = 2
-  max_epoch = 1
-  max_max_epoch = 1
-  keep_prob = 1.0
-  lr_decay = 0.5
-  batch_size = 20
-  vocab_size = 10000
+    """Tiny config, for testing."""
+    init_scale = 0.1
+    learning_rate = 1.0
+    max_grad_norm = 1
+    num_layers = 1
+    num_steps = 2
+    hidden_size = 2
+    max_epoch = 1
+    max_max_epoch = 1
+    keep_prob = 1.0
+    lr_decay = 0.5
+    batch_size = 20
+    vocab_size = 10000
 
 
 def run_epoch(session, model, input, eval_op=None, verbose=False):
-  """Runs the model on the given data."""
-  start_time = time.time()
-  for j,batch in enumerate(input):
+    """Runs the model on the given data."""
+    start_time = time.time()
+    for j,batch in enumerate(input):
 
-    questions,context,choices,labels,map,context_lens,qs_lens = batch
-    mapped_labels = ([model.labels_idx.index(x) for x in labels])
+        questions,context,choices,labels,map,context_lens,qs_lens = batch
+        mapped_labels = ([model.labels_idx.index(x) for x in labels])
 
-    state = session.run(model.initial_state)
+        state = session.run(model.initial_state)
 
-    fetches = {
-      "cost": model.cost,
-      "final_state": model.final_state,
-      "predictions":model.predictions,
-      "logits":model.logits,
-      "targets":model.targets,
-      "acc":model.acc
-    }
-    if eval_op is not None:
-        fetches["eval_op"] = eval_op
+        fetches = {
+          "cost": model.cost,
+          "final_state": model.final_state,
+          "predictions":model.predictions,
+          "logits": model.logits,
+          "targets": model.targets,
+          "acc": model.acc
+        }
+        if eval_op is not None:
+            fetches["eval_op"] = eval_op
 
-    for i,step in enumerate(context):
-        feed_dict = {}
-        feed_dict[model.initial_state] = state
-        feed_dict[model.input_x] = step
-        reshape_labels = np.array([x*np.ones(len(step[1])) for x in mapped_labels])
-        feed_dict[model.input_y]=reshape_labels
-        #feed_dict[model.sequence_lengths] = context_lens
-        seq_len = rn.get_seq_length(step)
-        feed_dict[model.sequence_lengths] = seq_len
+        for i, step in enumerate(context):
+            feed_dict = {}
+            feed_dict[model.initial_state] = state
+            feed_dict[model.input_x] = step
+            reshape_labels = np.array([x*np.ones(len(step[1])) for x in mapped_labels])
+            feed_dict[model.input_y]=reshape_labels
+            #feed_dict[model.sequence_lengths] = context_lens
+            seq_len = rn.get_seq_length(step)
+            feed_dict[model.sequence_lengths] = seq_len
 
-        vals = session.run(fetches, feed_dict)
-        cost = vals["cost"]
-        state = vals["final_state"]
-        if i==0:
-            print("batch %s; accuracy: %s"%(j,vals["acc"]))
+            vals = session.run(fetches, feed_dict)
+            cost = vals["cost"]
+            state = vals["final_state"]
+            if i==0:
+                print("batch %s; accuracy: %s"%(j,vals["acc"]))
 
-            print("predictions: %s"%vals["predictions"].T)
+                print("predictions: %s"%vals["predictions"].T)
 
 def get_config():
-  if FLAGS.model == "small":
-    return SmallConfig()
-  elif FLAGS.model == "medium":
-    return MediumConfig()
-  elif FLAGS.model == "large":
-    return LargeConfig()
-  elif FLAGS.model == "test":
-    return TestConfig()
-  else:
-    raise ValueError("Invalid model: %s", FLAGS.model)
+    if FLAGS.model == "small":
+        return SmallConfig()
+    elif FLAGS.model == "medium":
+        return MediumConfig()
+    elif FLAGS.model == "large":
+        return LargeConfig()
+    elif FLAGS.model == "test":
+        return TestConfig()
+    else:
+        raise ValueError("Invalid model: %s", FLAGS.model)
 
 
 def main(_):
 
-  train_path = os.path.join(FLAGS.data_wdw, 'test')
-  val_path = os.path.join(FLAGS.data_wdw, 'val')
-  test_path = os.path.join(FLAGS.data_wdw, 'test')
+    train_path = os.path.join(FLAGS.data_wdw, 'test')
+    val_path = os.path.join(FLAGS.data_wdw, 'val')
+    test_path = os.path.join(FLAGS.data_wdw, 'test')
 
-  config = get_config()
-  eval_config = get_config()
-  eval_config.batch_size = 1
-  eval_config.num_steps = 1
+    config = get_config()
+    eval_config = get_config()
+    eval_config.batch_size = 1
+    eval_config.num_steps = 1
 
-  q_steps = 20
-  c_steps = 300
-  config.num_steps = c_steps
+    q_steps = 20
+    c_steps = 300
+    config.num_steps = c_steps
 
-  print("Loading WDW Data..")
-  train = RawInput(rn.load_data(train_path))
+    print("Loading WDW Data..")
+    train = RawInput(rn.load_data(train_path))
 
-  print("loading iter data..")
-  train_iter = rn.batch_iter(
-      train.contexts, train.questions,
-      train.choices, train.labels, train.choices_map, train.context_lens,
-      train.qs_lens,batch_size=config.batch_size,
-      num_epochs=config.max_epoch, context_num_steps=c_steps,
-      question_num_steps=q_steps)
+    print("loading iter data..")
+    train_iter = rn.batch_iter(
+        train.contexts, train.questions,
+        train.choices, train.labels, train.choices_map, train.context_lens,
+        train.qs_lens,batch_size=config.batch_size,
+        num_epochs=config.max_epoch, context_num_steps=c_steps,
+        question_num_steps=q_steps)
 
-  with tf.Graph().as_default():
-    initializer = tf.random_uniform_initializer(-config.init_scale,
-                                                config.init_scale)
-    with tf.name_scope("Train"):
-      with tf.variable_scope("Model", reuse=None, initializer=initializer):
-        m = PTBModel(is_training=True, config=config,vocab_size=train.vocab_size,\
-                labels_idx=train.labels_idx,context_steps=c_steps,\
+    with tf.Graph().as_default():
+        initializer = tf.random_uniform_initializer(-config.init_scale,
+                                                    config.init_scale)
+        with tf.name_scope("Train"):
+            with tf.variable_scope("Model", reuse=None, initializer=initializer):
+                m = PTBModel(is_training=True, config=config,vocab_size=train.vocab_size,\
+                        labels_idx=train.labels_idx,context_steps=c_steps,\
                 question_steps = q_steps)
-      tf.scalar_summary("Training Loss", m.cost)
-      tf.scalar_summary("Learning Rate", m.lr)
+        tf.scalar_summary("Training Loss", m.cost)
+        tf.scalar_summary("Learning Rate", m.lr)
 
-    sv = tf.train.Supervisor(logdir=FLAGS.save_path)
-    with sv.managed_session() as session:
-      for i in range(config.max_max_epoch):
-        lr_decay = config.lr_decay ** max(i - config.max_epoch, 0.0)
-        m.assign_lr(session, config.learning_rate * lr_decay)
+        sv = tf.train.Supervisor(logdir=FLAGS.save_path)
+        with sv.managed_session() as session:
+            for i in range(config.max_max_epoch):
+                lr_decay = config.lr_decay ** max(i - config.max_epoch, 0.0)
+                m.assign_lr(session, config.learning_rate * lr_decay)
 
-        print("Epoch: %d Learning rate: %.3f" % (i + 1, session.run(m.lr)))
-        run_epoch(session, m, train_iter,eval_op=m.train_op,
-                                     verbose=True)
-      if FLAGS.save_path:
-        print("Saving model to %s." % FLAGS.save_path)
-        sv.saver.save(session, FLAGS.save_path, global_step=sv.global_step)
+                print("Epoch: %d Learning rate: %.3f" % (i + 1, session.run(m.lr)))
+                run_epoch(session, m, train_iter,eval_op=m.train_op,
+                          verbose=True)
+            if FLAGS.save_path:
+                print("Saving model to %s." % FLAGS.save_path)
+                sv.saver.save(session, FLAGS.save_path, global_step=sv.global_step)
 
 
 if __name__ == "__main__":
